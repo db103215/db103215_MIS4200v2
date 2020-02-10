7430e423b43ffcf187d40ad5e5c01b6db9260c50
Bryan <db103215@ohio.edu>
2020-02-10 17:55:42 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(db103215_MIS4200v2.Startup))]
+namespace db103215_MIS4200v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
