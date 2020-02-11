f2495a0598c29a5ac12a258d27748a961fad0e10
Dylan Bryan <db103215@ohio.edu>
2020-02-10 20:05:33 -0500

    adsf

---
+++ Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace db103215_MIS4200v2.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+        public ICollection<Course> Course { get; set; }
+    }
+
+    public class Course
+    {
+
+        [Key]
+        public int courseID { get; set; }
+        public string courseName { get; set; }
+        public string description { get; set; }
+
+        public ICollection<CourseDetail> CourseDetail { get; set; }
+
+        public int studentID{ get; set; }
+        public virtual Student Student { get; set; }
+
+    }
+
+    public class CourseDetail
+    {
+        public int courseDetailID { get; set; }
+        public int courseID { get; set; }
+        public virtual Course Course { get; set; }
+
+
+    }
+
+
+
+}
